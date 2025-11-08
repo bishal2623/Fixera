27fc71204b2fca427b8f5b7f9b2c8bcb305eddd1
Mohit Fegade <mohitfegade777@gmail.com>
2025-11-08 12:12:43 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/code-refactor/page.tsx
+++ src/app/code-refactor/page.tsx
@@ -1,4 +1,9 @@
-import CodeRefactor from '@/components/features/code-refactor';
+import dynamic from 'next/dynamic';
+
+const CodeRefactor = dynamic(
+  () => import('@/components/features/code-refactor'),
+  { ssr: false }
+);
 
 export default function CodeRefactorPage() {
   return <CodeRefactor />;
